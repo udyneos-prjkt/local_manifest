2c8ae95395208185d9c87a0c2adc212024d888bb
mnrdnn <dynemuhamad@gmail.com>
2026-05-08 07:54:33 +0700

    Update evox.sh

--- evox.sh
+++ evox.sh
@@ -58,7 +58,7 @@ echo -e "${Y}📦 Clone repo...${N}"
 git clone https://github.com/udyneos-prjkt/device_xiaomi_lancelot device/xiaomi/$DEV -b bka --depth=1
 git clone https://github.com/udyneos-prjkt/device_xiaomi_mt6768-common device/xiaomi/mt6768-common -b bka --depth=1
 git clone https://github.com/mk7x7/proprietary_vendor_xiaomi_lancelot vendor/xiaomi/$DEV -b 16.2 --depth=1
-git clone https://github.com/mk7x7/proprietary_vendor_xiaomi_mt6768-common.git vendor/xiaomi/mt6768-common -b 16.2 --depth=1
+git clone https://github.com/UdyneO2/proprietary_vendor_xiaomi_mt6768-common.git vendor/xiaomi/mt6768-common -b 16.2 --depth=1
 git clone https://github.com/MrShockWAVEog/ximi-lancerlin-krenlol.git kernel/xiaomi/mt6768 --depth=1 -b shockwave
 # hardware/xiaomi
 git clone https://github.com/crdroidandroid/android_hardware_xiaomi.git -b 16.0 hardware/xiaomi
